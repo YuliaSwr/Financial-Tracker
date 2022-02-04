2ec8cb0e45200ab5690d0f8eaa1df7264ced3fe8
Yuliia Sida <77783002+YuliaSwr@users.noreply.github.com>
2022-02-04 21:25:02 +0200

    s

---
+++ src/main/java/com/finance/security/PasswordValidator.java
@@ -0,0 +1,11 @@
+package com.finance.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordValidator {
+
+    public void check(String password) {
+
+    }
+}
